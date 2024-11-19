2ceade4a85275fae9f81ecfc81d27eb431a7deb0
vaniakk <vaniakaryadi@gmail.com>
2024-11-19 16:35:22 +0700

    base

--- resources/views/base/base.blade.php
+++ resources/views/base/base.blade.php
@@ -3,18 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cinema</title>
+    <title>LostAndFound</title>
     @vite('resources/css/app.css')
 </head>
 <body>
     
-
 <div class="container m-auto">
     @yield('content')
   </div>
-
-
-
     
 </body>
 </html>
