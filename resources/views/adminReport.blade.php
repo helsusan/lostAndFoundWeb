9373afbbdf4763a0c5513f1cd244e4b53362612e
vaniakk <vaniakaryadi@gmail.com>
2024-12-08 23:45:04 +0700

    update report request

--- resources/views/adminReport.blade.php
+++ resources/views/adminReport.blade.php
@@ -61,44 +61,46 @@
                             @endif
                         </td>
                         <td class="py-4 px-6 border-b @if($loop->odd) text-[#003366] @else text-white @endif font-medium text-center">{{ $report->is_verified ? 'Yes' : 'No' }}</td>
-                        <td class="py-4 px-6 border-b text-center flex flex-col space-y-2">
-                            @if (!$report->is_verified)
-                                <form action="{{ route('admin.isVerified', $report->id) }}" method="POST" class="w-full">
-                                    @csrf
-                                    @method('PUT')
-                                    <button type="submit" class="flex items-center justify-start bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 w-full font-bold">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                        <td class="py-4 px-6 border-b text-center">
+                            <div class="flex flex-col items-center gap-2">
+                                @if (!$report->is_verified)
+                                    <form action="{{ route('admin.isVerified', $report->id) }}" method="POST" class="w-full">
+                                        @csrf
+                                        @method('PUT')
+                                        <button type="submit" class="flex items-center justify-start bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 w-full font-bold">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                                            </svg>
+                                            Verify
+                                        </button>
+                                    </form>
+                                @else
+                                    <button type="button" class="flex items-center justify-start bg-gray-500 text-white px-4 py-2 rounded w-full font-bold" disabled="disabled">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                         </svg>
-                                        Verify
+                                        Verified
+                                    </button>
+                                @endif
+
+                                <a href="{{ route('admin.editReport', $report->id) }}" class="flex items-center justify-start bg-[#f3cf56] text-white px-4 py-2 rounded hover:bg-[#e6be40] w-full font-bold">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931ZM18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                    </svg>
+                                    Edit
+                                </a>
+
+                                <form action="{{ route('admin.deleteReport', $report->id) }}" method="POST" class="w-full">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="button" onclick="deleteRow(this)" class="buttondelete flex items-center justify-start  bg-[#f25c5c] text-white px-4 py-2 rounded hover:bg-[#d94a4a] w-full font-bold">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 mr-2">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                        </svg>
+                                        Delete
                                     </button>
                                 </form>
-                            @else
-                                <button type="button" class="flex items-center justify-start bg-gray-500 text-white px-4 py-2 rounded w-full font-bold" disabled="disabled">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                                    </svg>
-                                    Verified
-                                </button>
-                            @endif
-
-                            <a href="{{ route('admin.editReport', $report->id) }}" class="flex items-center justify-start bg-[#f3cf56] text-white px-4 py-2 rounded hover:bg-[#e6be40] w-full font-bold">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931ZM18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                                </svg>
-                                Edit
-                            </a>
-
-                            <form action="{{ route('admin.deleteReport', $report->id) }}" method="POST" class="w-full">
-                                @csrf
-                                @method('DELETE')
-                                <button type="button" onclick="deleteRow(this)" class="buttondelete flex items-center justify-start  bg-[#f25c5c] text-white px-4 py-2 rounded hover:bg-[#d94a4a] w-full font-bold">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 mr-2">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                    </svg>
-                                    Delete
-                                </button>
-                            </form>
+                            </div>
                         </td>
                     </tr>
                     @endforeach
