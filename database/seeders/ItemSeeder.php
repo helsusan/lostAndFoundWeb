15b101e3d9b6b617e5b2927953a34d9fac2c2e13
vaniakk <vaniakaryadi@gmail.com>
2024-12-12 14:00:11 +0700

    button color and seeder

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -15,7 +15,6 @@ class ItemSeeder extends Seeder
     {
         DB::table('items')->insert([
             [
-                'user_id'=> null,
                 'item_category_id' => 1,
                 'location_id' => 1,
                 'item_status_id' => 2,
@@ -28,7 +27,6 @@ class ItemSeeder extends Seeder
                 'updated_at' => now(),
             ],
             [
-                'user_id'=> null,
                 'item_category_id' => 2,
                 'location_id' => 2,
                 'item_status_id' => 2,
@@ -41,7 +39,6 @@ class ItemSeeder extends Seeder
                 'updated_at' => now(),
             ],
             [
-                'user_id'=> null,
                 'item_category_id' => 2,
                 'location_id' => 2,
                 'item_status_id' => 2,
@@ -54,7 +51,6 @@ class ItemSeeder extends Seeder
                 'updated_at' => now(),
             ],
             [
-                'user_id'=> null,
                 'item_category_id' => 2,
                 'location_id' => 2,
                 'item_status_id' => 2,
@@ -67,7 +63,6 @@ class ItemSeeder extends Seeder
                 'updated_at' => now(),
             ],
             [
-                'user_id'=> null,
                 'item_category_id' => 2,
                 'location_id' => 2,
                 'item_status_id' => 2,
@@ -80,7 +75,6 @@ class ItemSeeder extends Seeder
                 'updated_at' => now(),
             ],
             [
-                'user_id'=> null,
                 'item_category_id' => 2,
                 'location_id' => 2,
                 'item_status_id' => 2,
--- resources/views/adminReport.blade.php
+++ resources/views/adminReport.blade.php
@@ -50,12 +50,12 @@
                         <td class="py-4 px-6 border-b">
                             @if (is_null($report->item_id))
                                 <a href="{{ route('admin.showAssignPage', $report->id) }}" 
-                                class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 font-bold">
+                                class="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 font-bold">
                                     Assign
                                 </a>
                             @else
                                 <a href="{{ route('admin.detailItem', $report->item_id) }}" 
-                                class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 font-bold">
+                                class="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 font-bold">
                                     Detail
                                 </a>
                             @endif
@@ -67,7 +67,7 @@
                                     <form action="{{ route('admin.isVerified', $report->id) }}" method="POST" class="w-full">
                                         @csrf
                                         @method('PUT')
-                                        <button type="submit" class="flex items-center justify-start bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 w-full font-bold">
+                                        <button type="submit" class="flex items-center justify-start bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 w-full font-bold">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
                                             </svg>
@@ -75,7 +75,7 @@
                                         </button>
                                     </form>
                                 @else
-                                    <button type="button" class="flex items-center justify-start bg-gray-500 text-white px-4 py-2 rounded w-full font-bold" disabled="disabled">
+                                    <button type="button" class="flex items-center justify-start bg-gray-500 text-white px-4 py-2 rounded-lg w-full font-bold" disabled="disabled">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                         </svg>
@@ -83,7 +83,7 @@
                                     </button>
                                 @endif
 
-                                <a href="{{ route('admin.editReport', $report->id) }}" class="flex items-center justify-start bg-[#f3cf56] text-white px-4 py-2 rounded hover:bg-[#e6be40] w-full font-bold">
+                                <a href="{{ route('admin.editReport', $report->id) }}" class="flex items-center justify-start bg-yellow-500 hover:bg-yellow-600 text-white px-4 py-2 rounded-lg w-full font-bold">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931ZM18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                     </svg>
@@ -93,7 +93,7 @@
                                 <form action="{{ route('admin.deleteReport', $report->id) }}" method="POST" class="w-full">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="button" onclick="deleteRow(this)" class="buttondelete flex items-center justify-start  bg-[#f25c5c] text-white px-4 py-2 rounded hover:bg-[#d94a4a] w-full font-bold">
+                                    <button type="button" onclick="deleteRow(this)" class="buttondelete flex items-center justify-start  bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-lg w-full font-bold">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 mr-2">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                         </svg>
