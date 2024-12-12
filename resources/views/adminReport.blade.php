69d200295d59405247fc7b927979e724bc520d16
vaniakk <vaniakaryadi@gmail.com>
2024-12-12 16:43:25 +0700

    Data tables admin request

--- resources/views/adminReport.blade.php
+++ resources/views/adminReport.blade.php
@@ -2,38 +2,57 @@
 
 @section('content')
 
+<style>
+    div.dataTables_wrapper div.dataTables_length select {
+        width: 100px !important;
+    }
+
+    table tr:nth-child(odd) td {
+        background-color: #a9c6ff;
+        color: #003366;
+    }
+    table tr:nth-child(even) td {
+        background-color: #133E87;
+        color: #ffffff;
+    }
+
+    .rounded-lg {
+        overflow: hidden; 
+    }
+</style>
+
 <h1 class="text-center font-bold text-4xl my-10 text-[#133E87]">REPORT REQUEST</h1>
 
-<div class="shadow-lg rounded-lg overflow-hidden mx-4 md:mx-20 my-10 bg-[#f0f8ff]"> 
+<div class="overflow-hidden mx-4 md:mx-20 my-10"> 
     <div class="overflow-x-auto"> 
-        <table class="min-w-full table-auto border-collapse">
+        <table id="Table" class="min-w-full table-auto border-collapse rounded-lg">
             <thead>
-                <tr class="bg-[#133E87] text-left text-white"> 
-                    <th class="w-1/12 py-4 px-6 border-b text-sm font-bold uppercase text-center">ID</th>
-                    <th class="w-1/12 py-4 px-6 border-b text-sm font-bold uppercase text-center">Image</th>
-                    <th class="w-1/12 py-4 px-6 border-b text-sm font-bold uppercase text-center">Contact Person</th>
-                    <th class="w-1/12 py-4 px-6 border-b text-sm font-bold uppercase text-center">Description</th>
-                    <th class="w-1/12 py-4 px-6 border-b text-sm font-bold uppercase text-center">Location</th>
-                    <th class="w-1/12 py-4 px-6 border-b text-sm font-bold uppercase text-center">Location Detail</th>
-                    <th class="w-1/12 py-4 px-6 border-b text-sm font-bold uppercase text-center">Time Lost</th>
-                    <th class="w-1/12 py-4 px-6 border-b text-sm font-bold uppercase text-center">Status</th>
-                    <th class="w-1/12 py-4 px-6 border-b text-sm font-bold uppercase text-center">Item Status</th>
-                    <th class="w-1/12 py-4 px-6 border-b text-sm font-bold uppercase text-center">Verified</th>
-                    <th class="w-1/12 py-4 px-6 border-b text-sm font-bold uppercase text-center">Action</th>
+                <tr class="bg-[#133E87] text-center text-white"> 
+                    <th class="py-4 px-6 border-b text-sm font-bold uppercase text-center">ID</th>
+                    <th class="py-4 px-6 border-b text-sm font-bold uppercase text-center">Image</th>
+                    <th class="py-4 px-6 border-b text-sm font-bold uppercase text-center">Contact Person</th>
+                    <th class="py-4 px-6 border-b text-sm font-bold uppercase text-center">Description</th>
+                    <th class="py-4 px-6 border-b text-sm font-bold uppercase text-center">Location</th>
+                    <th class="py-4 px-6 border-b text-sm font-bold uppercase text-center">Location Detail</th>
+                    <th class="py-4 px-6 border-b text-sm font-bold uppercase text-center">Time Lost</th>
+                    <th class="py-4 px-6 border-b text-sm font-bold uppercase text-center">Status</th>
+                    <th class="py-4 px-6 border-b text-sm font-bold uppercase text-center">Item Status</th>
+                    <th class="py-4 px-6 border-b text-sm font-bold uppercase text-center">Verified</th>
+                    <th class="py-4 px-6 border-b text-sm font-bold uppercase text-center">Action</th>
                 </tr>
             </thead>
             <tbody>
                 @if($reports->isEmpty())
                     <tr>
-                        <td colspan="11" class="py-4 px-6 text-center text-white font-medium">
+                        <td colspan="11" class="py-4 px-6 text-center text-[#133E87] font-medium">
                             Table is empty. No reports found.
                         </td>
                     </tr>
                 @else
                     @foreach($reports as $report)
-                    <tr class="@if($loop->even) bg-[#133E87] @else bg-[#a9c6ff] @endif">
-                        <td class="py-4 px-6 border-b text-center @if($loop->odd) text-[#003366] @else text-white @endif font-medium text-center">{{ $report->id }}</td>
-                        <td class="py-4 px-6 border-b text-center">
+                    <tr>
+                        <td class="py-4 px-6 border-b text-left font-medium">{{ $report->id }}</td>
+                        <td class="py-4 px-6 border-b">
                             @if ($report->image)
                                 <img src="{{ asset($report->image) }}" alt="Image" class="rounded-lg cursor-pointer max-w-[120px] max-h-[120px] mx-auto"
                                      onclick="showModal('{{ asset($report->image) }}')">
@@ -41,26 +60,26 @@
                                 N/A
                             @endif
                         </td>
-                        <td class="py-4 px-6 border-b @if($loop->odd) text-[#003366] @else text-white @endif font-medium text-center">{{ $report->user->name ?? 'N/A' }}</td>
-                        <td class="py-4 px-6 border-b @if($loop->odd) text-[#003366] @else text-white @endif font-medium">{{ $report->description ?? 'N/A' }}</td>
-                        <td class="py-4 px-6 border-b @if($loop->odd) text-[#003366] @else text-white @endif font-medium text-center">{{ $report->location->name ?? 'N/A' }} - {{ $report->location->building ?? 'N/A' }}</td>
-                        <td class="py-4 px-6 border-b @if($loop->odd) text-[#003366] @else text-white @endif font-medium text-center">{{ $report->location_lost ?? 'N/A' }}</td>
-                        <td class="py-4 px-6 border-b @if($loop->odd) text-[#003366] @else text-white @endif font-medium text-center">{{ $report->time_lost ?? 'N/A' }}</td>
-                        <td class="py-4 px-6 border-b @if($loop->odd) text-[#003366] @else text-white @endif font-medium text-center">{{ $report->reportStatus->name ?? 'N/A' }}</td>
-                        <td class="py-4 px-6 border-b">
+                        <td class="py-4 px-6 border-b text-left font-medium">{{ $report->user->name ?? 'N/A' }}</td>
+                        <td class="py-4 px-6 border-b font-medium">{{ $report->description ?? 'N/A' }}</td>
+                        <td class="py-4 px-6 border-b text-left font-medium">{{ $report->location->name ?? 'N/A' }} - {{ $report->location->building ?? 'N/A' }}</td>
+                        <td class="py-4 px-6 border-b text-left font-medium">{{ $report->location_lost ?? 'N/A' }}</td>
+                        <td class="py-4 px-6 border-b text-center font-medium">{{ $report->time_lost ?? 'N/A' }}</td>
+                        <td class="py-4 px-6 border-b text-left font-medium">{{ $report->reportStatus->name ?? 'N/A' }}</td>
+                        <td class="py-4 px-6 border-b text-left">
                             @if (is_null($report->item_id))
                                 <a href="{{ route('admin.showAssignPage', $report->id) }}" 
-                                class="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 font-bold">
+                                   class="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 font-bold">
                                     Assign
                                 </a>
                             @else
                                 <a href="{{ route('admin.detailItem', $report->item_id) }}" 
-                                class="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 font-bold">
+                                   class="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 font-bold">
                                     Detail
                                 </a>
                             @endif
                         </td>
-                        <td class="py-4 px-6 border-b @if($loop->odd) text-[#003366] @else text-white @endif font-medium text-center">{{ $report->is_verified ? 'Yes' : 'No' }}</td>
+                        <td class="py-4 px-6 border-b text-left font-medium">{{ $report->is_verified ? 'Yes' : 'No' }}</td>
                         <td class="py-4 px-6 border-b text-center">
                             <div class="flex flex-col items-center gap-2">
                                 @if (!$report->is_verified)
@@ -93,9 +112,9 @@
                                 <form action="{{ route('admin.deleteReport', $report->id) }}" method="POST" class="w-full">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="button" onclick="deleteRow(this)" class="buttondelete flex items-center justify-start  bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-lg w-full font-bold">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 mr-2">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                    <button type="button" onclick="deleteRow(this)" class="buttondelete flex items-center justify-start bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-lg w-full font-bold">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-5 w-5 mr-2">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                                         </svg>
                                         Delete
                                     </button>
@@ -110,6 +129,7 @@
     </div>
 </div>
 
+
 <!-- Modal -->
 <div id="imageModal" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 hidden">
     <div class="relative bg-white rounded-lg p-4 w-11/12 max-w-4xl">
@@ -122,6 +142,12 @@
 </div>
 
 <script>
+    $('#Table').DataTable({
+        columnDefs: [
+            { orderable: false, targets: [1] }, 
+        ],
+    });
+
     function showModal(imageSrc) {
         const modal = document.getElementById('imageModal');
         const modalImage = document.getElementById('modalImage');
@@ -133,24 +159,6 @@
         const modal = document.getElementById('imageModal');
         modal.classList.add('hidden');
     }
-
-    document.getElementsByClassName("buttondelete").onclick = deleteRow;
-    function deleteRow(button) {
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "Deleted data cannot be reverted!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, Delete',
-            cancelButtonText: 'Cancel'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                button.parentElement.submit();
-            }
-        });
-    } 
 </script>
 
 @endsection
--- resources/views/base/base.blade.php
+++ resources/views/base/base.blade.php
@@ -25,8 +25,17 @@
               padding: 4px 8px;
           }
       </style>
-      <!-- SweetAlert2 -->
-       <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <!-- SweetAlert2 -->
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+        <!-- jquery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+
+        <!-- DataTables CSS -->
+        <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.min.css">
+
+        <!-- DataTables JS -->
+        <script src="https://cdn.datatables.net/2.1.8/js/dataTables.min.js"></script>
   </head>
   <body>
     @include('layouts.navigation')
@@ -38,4 +47,35 @@
     @yield('home')
   </body>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script>
+        $(document).ready(function() {
+            $('#Table').DataTable({
+                paging: true,
+                searching: true,
+                ordering: true,
+                info: true,
+                lengthChange: true,
+                scrollX: true,
+                autoWidth: false,
+            });
+        });
+
+        document.getElementsByClassName("buttondelete").onclick = deleteRow;
+        function deleteRow(button) {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "Deleted data cannot be reverted!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, Delete',
+            cancelButtonText: 'Cancel'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                button.parentElement.submit();
+            }
+        });
+    } 
+    </script>
 </html>
