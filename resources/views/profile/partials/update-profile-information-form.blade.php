03c27ca8a0b874fa9a16eacf9302b47b5398a765
helsusan <helenasusan.tanoyo@gmail.com>
2024-12-14 17:52:51 +0700

    profile

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -47,6 +47,12 @@
             @endif
         </div>
 
+        <div>
+            <x-input-label for="phone_number" :value="__('phone_number')" />
+            <x-text-input id="phone_number" name="phone_number" type="text" class="mt-1 block w-full" :value="old('phone_number', $user->phone_number)" required autofocus autocomplete="phone_number" />
+            <x-input-error class="mt-2" :messages="$errors->get('phone_number')" />
+        </div>
+
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
 
