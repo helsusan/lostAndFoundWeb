9660fcd0ef6f4785f4959d527e22751cde29e232
aikoolee <aikolee2023@gmail.com>
2024-12-14 11:23:39 +0700

    update delete button item

--- resources/views/adminItem.blade.php
+++ resources/views/adminItem.blade.php
@@ -117,6 +117,7 @@
                         </td>
                         <td class="py-4 px-6 border-b text-center">
                         <div class="flex flex-col items-center gap-2"></div>
+                            <!-- edit -->
                             <a href="{{ route('admin.editItem', $item->id) }}" 
                             class="flex items-center justify-start bg-yellow-500 hover:bg-yellow-600 text-white px-4 py-2 rounded-lg w-full font-bold mb-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -124,17 +125,16 @@
                                 </svg>
                                 Edit
                             </a>
-
+                            <!-- delete -->
                             <form action="{{ route('admin.deleteItem', $item->id) }}" method="POST" class="w-full">
                                 @csrf
                                 @method('DELETE')
-                                <button type="button" class="button-delete flex items-center justify-start bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-lg w-full font-bold">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                </svg>
-                                Delete
-                            </button>
-
+                                <button type="button" onclick="deleteRow(this)" class="buttondelete flex items-center justify-start bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded min-w-[120px] font-bold">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 mr-2">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                        </svg>
+                                            Delete
+                                </button>
                             </form>
                             </div>
                         </td>
@@ -223,29 +223,6 @@
                         text: 'An error occurred while updating the item status.',
                     });
                 });
-        });
-    });
-
-    // delete
-    document.querySelectorAll('.button-delete').forEach(function(button) {
-        button.addEventListener('click', function(e) {
-            e.preventDefault(); 
-            const form = this.closest('form'); 
-
-            Swal.fire({
-                title: 'Are you sure?',
-                text: "Deleted data cannot be reverted!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Yes, Delete',
-                cancelButtonText: 'Cancel'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    form.submit(); // submit apabila sudah confirmed
-                }
-            });
         });
     });
 </script>
