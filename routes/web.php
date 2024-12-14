e04c14de5a02a09e84d43083970a4bec072b1efd
helsusan <helenasusan.tanoyo@gmail.com>
2024-12-14 17:51:26 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@ Route::get('/', function () {
     return redirect()->route('login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     // home
